53eef087fa0a1fe679a1eda39620c265855561a5
Rafael Monico <rmonico1@gmail.com>
2019-01-24 22:50:35 -0200

    Created tags property on Task

---
+++ src/main/java/br/zero/txtask/core/model/Tag.java
@@ -0,0 +1,13 @@
+package br.zero.txtask.core.model;
+
+public class Tag {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/br/zero/txtask/core/model/Task.java
+++ src/main/java/br/zero/txtask/core/model/Task.java
@@ -1,7 +1,15 @@
 package br.zero.txtask.core.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
     private String title;
+    private List<Tag> tags;
+
+    public Task() {
+        tags = new ArrayList<>();
+    }
 
     public String getTitle() {
         return title;
@@ -10,4 +18,9 @@ public class Task {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
 }
