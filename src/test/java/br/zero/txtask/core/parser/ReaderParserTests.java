8cc7119189cf6e49a05982764801a6b91e4e047b
Rafael Monico <rmonico1@gmail.com>
2019-01-24 22:53:46 -0200

    New test on task tags

--- src/test/java/br/zero/txtask/core/parser/ReaderParserTests.java
+++ src/test/java/br/zero/txtask/core/parser/ReaderParserTests.java
@@ -52,4 +52,18 @@ public class ReaderParserTests {
         assertThat(list, task(2).title(is("Third task")));
     }
 
+    @Test
+    public void should_parse_task_tags() throws FileNotFoundException, ParserException {
+        Parser parser = new ReaderParser(new FileReader("src/test/resources/should_parse_task_tags.txk"));
+
+        TaskList list = parser.parse();
+
+        assertThat(list, title(is("A list to test tags on tasks")));
+
+        assertThat(list, taskCount(is(2)));
+        assertThat(list, task(0).title(is("Task with no tags")));
+        assertThat(list, task(1).title(is("Another task")));
+        assertThat(list, task(1).tagCount(is(1)));
+        assertThat(list, task(1).tag(0).name(is("this_time_with_1_tag.dots_and_numb3rs_are_allowed_too")));
+    }
 }
