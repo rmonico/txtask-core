10ea96ecbf46c25476789933ba63974e56413b4f
Rafael Monico <rmonico1@gmail.com>
2019-01-28 17:33:02 -0200

    Indenting with spaces instead tabs

--- src/main/java/br/zero/txtask/core/parser/ParserException.java
+++ src/main/java/br/zero/txtask/core/parser/ParserException.java
@@ -1,16 +1,16 @@
 package br.zero.txtask.core.parser;
 
 public class ParserException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1771024495164039959L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1771024495164039959L;
 
-	public ParserException(Throwable e) {
-		super(e);
-	}
+    public ParserException(Throwable e) {
+        super(e);
+    }
 
-	public ParserException(final String message) {
-		super(message);
-	}
+    public ParserException(final String message) {
+        super(message);
+    }
 }
