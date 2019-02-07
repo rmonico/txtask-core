9240e8e99f86061c45fe85294d141c7ed28e909d
Rafael Monico <rmonico1@gmail.com>
2019-02-07 02:49:24 -0200

    Not creating scopes from Scope interface directly, findParser moved to Scope interface and renamed to findScope

--- src/main/java/br/zero/txtask/core/parser/Scope.java
+++ src/main/java/br/zero/txtask/core/parser/Scope.java
@@ -1,19 +1,17 @@
 package br.zero.txtask.core.parser;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 import br.zero.txtask.core.parser.element.ElementMatcher;
 import br.zero.txtask.core.parser.element.ElementParser;
+import br.zero.txtask.core.parser.reader.ParserReader;
 
 public interface Scope<T> {
 
-    public static <T> Scope<T> createDescription(ElementMatcher matcher,
-            ElementParser<T> parser,
-            Consumer<T> consumer) {
-        return new AbstractScope<T>(matcher, parser, consumer);
-    }
-
     public ElementMatcher getMatcher();
+
+    public Scope<?> findScope(ParserReader reader) throws ParserException, IOException;
 
     public ElementParser<T> getParser();
 
