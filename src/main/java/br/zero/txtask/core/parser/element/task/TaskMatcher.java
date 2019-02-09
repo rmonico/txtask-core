30c2bc311ead30a1d59a8c91ed8338560732485d
Rafael Monico <rmonico1@gmail.com>
2019-02-09 04:22:41 -0200

    Refactor

--- src/main/java/br/zero/txtask/core/parser/element/task/TaskMatcher.java
+++ src/main/java/br/zero/txtask/core/parser/element/task/TaskMatcher.java
@@ -1,17 +1,16 @@
 package br.zero.txtask.core.parser.element.task;
 
-import static br.zero.txtask.core.parser.element.task.Constants.TASK_STATUSES_ARRAY;
+import br.zero.txtask.core.parser.element.ElementMatcher;
+import br.zero.txtask.core.parser.reader.ParserReader;
 
 import java.io.IOException;
 
-import br.zero.txtask.core.parser.ParserException;
-import br.zero.txtask.core.parser.element.ElementMatcher;
-import br.zero.txtask.core.parser.reader.ParserReader;
+import static br.zero.txtask.core.parser.element.task.Constants.TASK_STATUSES_ARRAY;
 
 public class TaskMatcher implements ElementMatcher {
 
     @Override
-    public boolean matchs(ParserReader reader) throws ParserException, IOException {
+    public boolean matchs(ParserReader reader) throws IOException {
         return reader.followed().byAnyOf(TASK_STATUSES_ARRAY).which() > -1;
     }
 
